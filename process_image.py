9bc67ed0243eb3e2cede326980b4d5730e14a4a7
hadar <hadar.strauss@mail.huji.ac.il>
2021-05-06 22:47:21 +0300

    changes to my file

--- process_image.py
+++ process_image.py
@@ -1,22 +1,24 @@
 import cv2
+from os import remove
+from pathlib import Path
 from process_assist import process
 
 TEMP = "temp.png"
 LAB_IMAGES = "lab_images/"
-PATH = "C:/Users/97250/Desktop/LAB_B2/advanced_lab_PYTON/"
+PATH = str(Path().absolute()) + '\\'
 
 
 def get_B_H_point(B=0, exposer_time=-6):
     cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
     cam.set(cv2.CAP_PROP_EXPOSURE, exposer_time)
-    # todo:
     ret, frame = cam.read()
     if not ret:
         print('failed to grab image')
         return
-    # frame = cv2.imread('test.png', 2)  # todo temp
+    # frame = cv2.imread('test.png', 2)  # test
     cv2.imwrite(PATH + TEMP, frame)
     name = process(PATH, TEMP, B)
+    remove(PATH + TEMP)  # remove temp
     cv2.imwrite(PATH + LAB_IMAGES + name, frame)
 
 
