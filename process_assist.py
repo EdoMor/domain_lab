650bf222f453d64d90cb0687f2d582be074e6273
hadar <hadar.strauss@mail.huji.ac.il>
2021-05-24 03:55:53 +0300

    1. way to process images changed 2. way to save images changed

--- process_assist.py
+++ process_assist.py
@@ -1,10 +1,29 @@
 from PIL import Image, ImageEnhance
 import numpy as np
 import datetime
+import cv2
 
 
-def enhance(img: Image) -> Image:
-    chopsize = 50
+def gray_and_blurr(img, blurr: bool = True):
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    if blurr:
+        gray_img = cv2.GaussianBlur(gray_img, (11, 11), 0)
+    Img = Image.fromarray(gray_img)
+    return Img
+
+
+def thresh_binary(img, thresh: int = None):
+    b = np.average(np.array(img))
+    c = b + (80 - b) / 10
+    threshold = c if thresh is None else thresh
+    _, img_thres = cv2.threshold(np.asarray(img), threshold, 255, cv2.THRESH_BINARY)
+    kernel = np.ones((2, 2), np.uint8)
+    img_op = cv2.morphologyEx(img_thres, cv2.MORPH_OPEN, kernel, iterations=1)
+    return Image.fromarray(img_op)
+
+
+def new_enhance(img, chopsize: int = 50):
+    img = gray_and_blurr(img)
     width, height = img.size
     for x0 in range(0, width, chopsize):
         for y0 in range(0, height, chopsize):
@@ -12,27 +31,23 @@ def enhance(img: Image) -> Image:
             y1 = y0 + chopsize if y0 + chopsize < height else height - 1
             box = (x0, y0, x1, y1)
             slc = img.crop(box)
-            slc = ImageEnhance.Contrast(slc).enhance(1000)
+            slc = thresh_binary(slc)
             img.paste(slc, (x0, y0))
-    img = ImageEnhance.Contrast(img).enhance(100)
-    fn = lambda x: 255 if x > 100 else 0
-    img = img.convert('L').point(fn, mode='1')
     return img
 
 
 def file_name_T_H_M(img: Image, H: float, counter=None) -> tuple:
-    now = datetime.datetime.now()
     # str(now.month) + '_' + str(now.day) + '_' +
+    now = datetime.datetime.now()
     time = str(now.day) + '_' + str(now.time()).replace('.', '_').replace(':', '_')
     time = time if counter is None else counter
     M = np.average(np.array(img))
 
-    return f'{time}_{H}_{M}.png', f'{time}_{H}_.png'
+    return f'C{time}_H{H}_M{M}.png', f'C{time}_H{H}_M{M}.png'
 
 
-def process(path, temp, H: float, folder_name: str, counter=None) -> str:
-    img = Image.open(path + temp)
-    img = enhance(img)
+def process(path, folder_name: str, img, H: float, counter=None) -> str:
+    img = new_enhance(img)
     name_p, name_r = file_name_T_H_M(img, H, counter)
     img.save(path + folder_name + name_p)
     return name_r
