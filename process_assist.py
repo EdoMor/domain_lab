e416465dc34064cac52444bd9ddbf145557ae9bf
hadar <hadar.strauss@mail.huji.ac.il>
2021-06-06 04:36:22 +0300

    made fn that scan for unprocessed runs and processes them

--- process_assist.py
+++ process_assist.py
@@ -1,7 +1,14 @@
-from PIL import Image, ImageEnhance
+from PIL import Image
 import numpy as np
 import datetime
 import cv2
+import os
+import constants
+
+RAW = constants.RAW
+PROCESSED = constants.PROCESSED
+join = os.path.join
+
 
 def detect_color(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -13,7 +20,7 @@ def detect_color(img):
 
 def clear_dots(img):
     kernel = np.ones((2, 2), np.uint8)
-    s=1
+    s = 1
     if np.average(np.array(img)) > 127.5:
         img_op1 = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel, iterations=s)
         img_op2 = cv2.morphologyEx(img_op1, cv2.MORPH_CLOSE, kernel, iterations=s)
@@ -32,9 +39,34 @@ def file_name_T_H_M(img: Image, H: float, counter=None) -> str:
     return f'C{time}_H{H}_M{M}.png'
 
 
-def process(path, folder_name: str, img, H: float, counter=None) -> str:
-    img = detect_color(img)
-    img = clear_dots(img)
-    name = file_name_T_H_M(img, H, counter)
-    cv2.imwrite(path + folder_name + name, img)
-    return name
+def process(run_path: str):
+    for filename in os.listdir(join(run_path, RAW)):
+        img = cv2.imread(join(run_path, RAW, filename))
+        img = detect_color(img)
+        img = clear_dots(img)
+        cv2.imwrite(join(run_path, PROCESSED, filename), img)
+
+
+def is_empty_folder(path: str) -> bool:
+    if os.path.exists(path) and os.path.isdir(path):
+        if not os.listdir(path):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def scan_and_process(runs_path: str = './runs'):  # TODO: Deal with folders that are half full
+    processed_set = set()
+    while True:
+        unprocessed_set = set(os.listdir(runs_path))-processed_set
+        for filename in unprocessed_set:
+            if is_empty_folder(join(runs_path, filename, PROCESSED)):
+                process(join(runs_path, filename, PROCESSED))
+                processed_set.add(filename)
+# TODO: get processed_set (after forced ending)
+
+
+if __name__ == '__main__':
+    scan_and_process()
