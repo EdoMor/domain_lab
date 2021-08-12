256941293e97713c269dbdf0067778abfe3c3f76
hadar <hadar.strauss@mail.huji.ac.il>
2021-08-12 23:10:33 +0300

    added pca file- has fn that terns imgs to gray scale in a smart way

--- pca.py
+++ pca.py
@@ -1,28 +1,21 @@
-from sklearn.decomposition import PCA
-from PIL import Image
-from numpy import asarray
 import numpy as np
-import cv2
-import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 
-def smart_Gscale(img):
-    # open image
-    data = asarray(img)[:, :, [0, 1, 2]]
-
+def smart_Gscale(img: np.array) -> np.array:
     # save shape
-    shape = data.shape[:2]
+    shape = img.shape[:2]
 
-    # smart grey scale
     # to vector
-    X = data.reshape(-1, 3)
-    # project to 1D
-    pca = PCA(n_components=1).fit(X)
-    X = pca.transform(X).reshape(-1)
-    # normalize
-    m = min(X)
-    M = max(X)-m
-    X = np.uint8((X-m)*255//M)
-    # to matrix
-    data = X.reshape(shape)
+    v = img.reshape(-1, 3)
 
+    # project to 1D
+    pca = PCA(n_components=1).fit(v)
+    v = pca.transform(v).reshape(-1)
+    # normalize
+    m = min(v)
+    M = max(v) - m
+    v = np.uint8((v - m) * 255 // M)
+
+    # to matrix
+    return v.reshape(shape)
