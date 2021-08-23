7b65e5b9e79d410ebdc86be355b6aa576eb2b5a9
hadar <hadar.strauss@mail.huji.ac.il>
2021-08-23 04:20:22 +0300

    added exeptions to data_generator

--- datagenerator.py
+++ datagenerator.py
@@ -13,7 +13,7 @@ def get_volt_from_img_name(name:str)->float:
     return float(name.split('_')[-1].replace('.png',''))
 
 
-def data_generator(inputDataPath, outputDataPath, batchSize, resized_width=None, resized_height=None):
+def data_generator(inputDataPath:str, outputDataPath:str, batchSize:int, train_mode:bool=True,  resized_width:int=None, resized_height:int=None):
     '''
     :return: a generator of a bach of data(input,output) from the dataPath dir,
              in the size of batchSize.
@@ -25,11 +25,21 @@ def data_generator(inputDataPath, outputDataPath, batchSize, resized_width=None,
     a = os.scandir(inputDataPath)
     b = os.scandir(outputDataPath)
     while True:
-        input = []
-        output = []
-        while len(input) < batchSize:
-            input_file_name = next(a).name
-            output_file_name = next(b).name
+        input = [0]*batchSize
+        output = [0]*batchSize
+        for i in range(batchSize):
+            try:
+                input_file_name = next(a).name
+                output_file_name = next(b).name
+            except StopIteration:
+                if train_mode:
+                    a = os.scandir(inputDataPath)
+                    b = os.scandir(outputDataPath)
+                    input_file_name = next(a).name
+                    output_file_name = next(b).name
+                else:
+                    raise StopIteration
+
             input_img = cv2.imread(os.path.join(inputDataPath,input_file_name), cv2.IMREAD_GRAYSCALE)
             output_img = cv2.imread(os.path.join(outputDataPath,output_file_name), cv2.IMREAD_GRAYSCALE)
             # ensures all images be the same size:
@@ -39,15 +49,15 @@ def data_generator(inputDataPath, outputDataPath, batchSize, resized_width=None,
             input_volt = get_volt_from_img_name(input_file_name)
             output_volt = get_volt_from_img_name(output_file_name)
             # flatten images to vectors, and adds vols to input:
-            input.append(np.append(input_img.flatten(),
-                            [input_volt, output_volt, output_volt-input_volt]))
-            output.append(output_img.flatten())
+            input[i] = np.append(input_img.flatten(),
+                            [input_volt, output_volt, output_volt-input_volt])
+            output[i] = output_img.flatten()
         yield input, output
 
 
-
-
 if __name__ == '__main__':
-    gen = data_generator('./data/train/input', './data/train/output', 2)
-    print(next(gen))
-    print(next(gen))
+    for i,j in enumerate(data_generator('./data/train/input', './data/train/output', 2)):
+        print(j)
+        break
+    # print(next(gen))
+    # print(next(gen))
