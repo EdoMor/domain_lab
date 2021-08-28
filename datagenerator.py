28ebaa23fd74c6c09b860b654e547b77f5466978
hadar <hadar.strauss@mail.huji.ac.il>
2021-08-28 03:45:00 +0300

    added 'next_data' gen, that gets the next data from all the run files. changed 'data_generator' gen to get data from insted of os. added helper functions. did not do tests yet! -expect fixes

--- datagenerator.py
+++ datagenerator.py
@@ -13,51 +13,90 @@ def get_volt_from_img_name(name:str)->float:
     return float(name.split('_')[-1].replace('.png',''))
 
 
-def data_generator(inputDataPath:str, outputDataPath:str, batchSize:int, train_mode:bool=True,  resized_width:int=None, resized_height:int=None):
-    '''
-    :return: a generator of a bach of data(input,output) from the dataPath dir,
-             in the size of batchSize.
+def reshape_for_net(img, resized_width:int=None, resized_height:int=None):
+    # ensures all images be the same size:
+    if resized_height is not None:
+        img = cv2.resize(img, (resized_width, resized_height))
+    # tern img to zeros and ones:
+    _img = img / 255
+    # flatten img to a vector
+    return img.flatten()
+
+
+def add_volts(reshaped_img, input_volt:float, output_volt:float):
+    return np.append(reshaped_img, [input_volt, output_volt, output_volt - input_volt])
+
+
+def img_to_input(img, input_volt:float, output_volt:float, resized_width:int=None, resized_height:int=None):
+    reshaped_img = reshape_for_net(img, resized_width, resized_height)
+    return add_volts(reshaped_img, input_volt, output_volt)
+
+
+def img_from_input(input:np.array, resized_width:int, resized_height:int) -> np.array:
+    flattened_img = np.delete(input, [-1, -2, -3])
+    return flattened_img.reshape([resized_width, resized_height])
+
+
+def img_from_output(output:np.array, resized_width:int, resized_height:int) -> np.array:
+    return output.reshape([resized_width, resized_height])
+
+
+def next_data(all_runs_path:str, resized_width:int=None, resized_height:int=None):
+    """
+    :return: the next data pair: (input, output)
              the data will all resize to dim : (resized_width, resized_height),
-             then flattend.
+             then flatten.
              the input is a flattend img + input and output volteges + their subtraction
              the output is is a flattend img
-    '''
-    a = os.scandir(inputDataPath)
-    b = os.scandir(outputDataPath)
+    """
     while True:
-        input = [0]*batchSize
-        output = [0]*batchSize
+        for run in os.listdir(all_runs_path):
+            run_path = os.path.join(all_runs_path, run)
+            run_iter = os.scandir(run_path)
+            try:
+                input_img_name = next(run_iter).name
+                input_volt = get_volt_from_img_name(input_img_name)
+                input_img_path = os.path.join(run_path, input_img_name)
+                reshaped_input = reshape_for_net(cv2.imread(input_img_path, cv2.IMREAD_GRAYSCALE), resized_width, resized_height)
+            except StopIteration:
+                break
+            while True:
+                try:
+                    output_img_name = next(run_iter).name
+                    output_volt = get_volt_from_img_name(output_img_name)
+                    output_img_path = os.path.join(run_path, output_img_name)
+                    output = reshape_for_net(cv2.imread(output_img_path, cv2.IMREAD_GRAYSCALE), resized_width,
+                                                        resized_height)
+                    input = add_volts(reshaped_input, input_volt, output_volt)
+                    yield input, output
+                    input_volt = output_volt
+                    reshaped_input = output
+                except StopIteration:
+                    break
+
+
+def data_generator(all_runs_path:str, batchSize:int, train_mode:bool=True):
+    '''
+    :return: a bach of data(input,output) from the all_runs_path dir,
+             in the size of batchSize.
+    '''
+
+    data_iter = next_data(all_runs_path)
+    while True:
+        inputs = [0]*batchSize
+        outputs = [0]*batchSize
         for i in range(batchSize):
             try:
-                input_file_name = next(a).name
-                output_file_name = next(b).name
+                inputs[i], outputs[i] = next(data_iter)
             except StopIteration:
                 if train_mode:
-                    a = os.scandir(inputDataPath)
-                    b = os.scandir(outputDataPath)
-                    input_file_name = next(a).name
-                    output_file_name = next(b).name
+                    data_iter = next_data(all_runs_path)
+                    inputs[i], outputs[i] = next(data_iter)
                 else:
                     raise StopIteration
-
-            input_img = cv2.imread(os.path.join(inputDataPath,input_file_name), cv2.IMREAD_GRAYSCALE)
-            output_img = cv2.imread(os.path.join(outputDataPath,output_file_name), cv2.IMREAD_GRAYSCALE)
-            # ensures all images be the same size:
-            if resized_height is not None:
-                input_img = cv2.resize(input_img, (resized_width, resized_height))
-                output_img = cv2.resize(output_img, (resized_width, resized_height))
-            input_volt = get_volt_from_img_name(input_file_name)
-            output_volt = get_volt_from_img_name(output_file_name)
-            # flatten images to vectors, and adds vols to input:
-            input[i] = np.append(input_img.flatten(),
-                            [input_volt, output_volt, output_volt-input_volt])
-            output[i] = output_img.flatten()
-        yield input, output
+        yield inputs, outputs
 
 
 if __name__ == '__main__':
-    for i,j in enumerate(data_generator('./data/train/input', './data/train/output', 2)):
-        print(j)
-        break
-    # print(next(gen))
-    # print(next(gen))
+
+
