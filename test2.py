c4a22efd4383adbc2b960574216b55907027c859
hadar <hadar.strauss@mail.huji.ac.il>
2021-09-29 22:18:10 +0300

    add 'my_fit' fn

--- test2.py
+++ test2.py
@@ -6,97 +6,105 @@ import os
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import random
+import tensorflow as tf
+from tensorflow import keras
+from datagenerator import *
 
 
-# my_run = r'F:\pycharm projects\domain lab\runs\run_8\raw'
-# pics = os.listdir(my_run)
+# constants:
+SIZE = 160
+RESIZED_WIDTH = SIZE  # temp
+RESIZED_HEIGHT = SIZE  # temp
+RESIZED_NUM_PIXELS = RESIZED_WIDTH * RESIZED_HEIGHT
+SHAPE = RESIZED_NUM_PIXELS + 3
+TRAIN_PATH = r'R:\domain_lab\runs_processed\train'  # temp
+VALID_PATH = r'R:\domain_lab\runs_processed\validation'  # temp
+TEST_PATH = r'R:\domain_lab\runs_processed\test'  # temp
+BACH_SIZE = 13 # temp
+TRAIN_DATASET_SIZE = dataset_size(TRAIN_PATH)
+VALID_DATASET_SIZE = dataset_size(VALID_PATH)
+TEST_DATA_SIZE = dataset_size(TEST_PATH)
+STEPS_PER_EPOCH = TRAIN_DATASET_SIZE // BACH_SIZE
+EPOCHS = 10  # temp
+VALIDATION_STEPS = VALID_DATASET_SIZE // BACH_SIZE
+TEST_STEPS = TEST_DATA_SIZE // BACH_SIZE
+
+
+model = tf.keras.Model
+
+
+def my_fit(train_inputs, train_output, valid_inputs, valid_output, batch_size=32, epochs: int = 1):
+    """
+    inputs: generator of a list of np.array inputs
+    output: generator of a np.array output
+
+    """
+    for _ in range(epochs):
+        flag = True
+        while flag:
+            try:
+                next_bach_of_training_inputs = next(train_inputs)
+                next_bach_of_training_output = next(train_output)
+                validation_data = (next(valid_inputs), next(valid_output))
+                model.fit(x = next_bach_of_training_inputs,
+                          y = next_bach_of_training_output,
+                          batch_size = batch_size,
+                          use_multiprocessing = True)
+            except StopIteration:
+                while flag:
+                    try:
+                        next_bach_of_valid_inputs = next(valid_inputs)
+                        next_bach_of_valid_output = next(valid_output)
+                        model.evaluate(x = next_bach_of_valid_inputs,
+                                       y = next_bach_of_valid_output,
+                                       batch_size=batch_size,
+                                       use_multiprocessing=True)
+                    except StopIteration:
+                       flag = False
+
+# #test:
 #
-# volts=[]
+# train_input_data = random.sample(range(-100, 100), 1000)
+# train_output_data = map(lambda x: x**2, input_data)
 #
-# for i, pic_name in enumerate(pics):
-#     volts.append(float(pic_name.split('_')[-1].replace('.png','')))
-
-
-# x=range(len(volts))
-# plt.plot(x,volts)
-# plt.show()
-
-
-# img = mpimg.imread(r'F:\pycharm projects\domain lab\runs\run_8\raw\1628506804.1056578_0.0.png')
-# imgplot = plt.imshow(img)
-# plt.show()
-
-# left = 559
-# top = 65
-# right = 1380
-# bottom = 906
+# valid_input_data = random.sample(range(-100, 100), 1000)
+# # valid_output_data = map(lambda x: x**2, input_data)
 #
-# for pic_name in pics:
-#     pic = Image.open(os.path.join(my_run, pic_name)).crop((left, top, right, bottom))
-#     pic.save(os.path.join(r'F:\pycharm projects\domain lab\new_sample',pic_name))
-
-# M=[]
-# new_sample_drc = r'F:\pycharm projects\domain lab\new_sample'
-# images = os.listdir(new_sample_drc)
-# for img in images:
-#     pic = Image.open(os.path.join(new_sample_drc, img))
-#     picGray = pic.convert('L')
-#     M.append(np.average(np.array(picGray)))
 #
-# plt.plot(volts,M)
-# plt.show()
+# def test_gen_input(input:bool, batch_size):
+#     count = 0
+#     while True:
+#         yield np.array(train_input_data[count:count+batch_size])
+#         count += batch_size
+#
+# def test_gen_output(input:bool, batch_size):
+#     count = 0
+#     while True:
+#         yield np.array(train_output_data[count:count+batch_size])
+#         count += batch_size
+#
+# # model:
+# model = tf.keras.Sequential()
+# model.add(tf.keras.Input(shape=1,))
+# model.add(tf.keras.layers.Dense(5))
+# model.add(tf.keras.layers.Dense(1))
+#
+# # Compile the Model:
+# model.compile(optimizer=keras.optimizers.Adam())
+#
+#
+# x_generator = test_gen_input()
+# y_generator
+#
+#
+# # Training the Model:
+# model.fit(x=train_generator,
+#           steps_per_epoch=STEPS_PER_EPOCH,
+#           epochs=EPOCHS,
+#           validation_data=validation_generator,
+#           validation_steps=VALIDATION_STEPS)
+#
+#
+#
 
-# img = mpimg.imread(r'F:\pycharm projects\domain lab\runs\run_121628590016.6035893_0.0.png')
-# imgplot = plt.imshow(img)
-# plt.show()
-
-# def loud_data(runs_path: str = './runs'):
-#     runs = os.listdir(runs_path)
-#     data = np.empty([1, len(runs)])
-#     T_H_B_of_data = np.empty([1, len(runs)])
-#     for i, run_folder in enumerate(runs):
-#         run_images_path = join(runs_path, run_folder, JPROCESSED)
-#         run_images = os.listdir(run_images_path)
-#         run_data = np.empty([1, len(run_images)])
-#         T_H_B_of_run_data = np.empty([1, len(run_images)])
-#         for j, img in enumerate(run_images):
-#             run_data[j] = np.asarray(Image.open(join(run_images_path, img)))
-#             T_H_B_of_run_data[j] = img.split('_')
-#         data[i] = run_data
-#         T_H_B_of_run_data[i] = T_H_B_of_run_data
-#     return data, T_H_B_of_data
-
-
-def data(runs_path: str = './runs'):
-    runs = os.listdir(runs_path)
-    input_img = np.empty([1, len(runs)*999])
-    output_img = np.empty([1, len(runs)*999])
-    jump = np.empty([1, len(runs)*999])
-    for run in runs:
-        run_images = os.listdir(os.path.join(runs_path,run))
-        for img in run_images:
-
-
-
-# jumps = np.array(random.sample(range(1, 50000), 1000)) / 55000
-# s =5*np.sin(jumps)
-# w=np.delete(s,0)
-# w=np.append(w,0)
-# n=s-w
-# n = np.delete(n,-1)
-# print(max(n))
-# print(min(abs(n)))
-
-
-# img = cv2.imread(r'F:\pycharm projects\domain lab\runs\run_14\1628590841.3998182_0.0.png')
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# cv2.imwrite(r'./test_gray_size.png', gray)
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
